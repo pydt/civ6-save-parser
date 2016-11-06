3f07412f400978504fda66a84f595609b82b5ff5
Mike Rosack <mrosack@gmail.com>
2016-11-05 21:19:19 -0500

    new parsing method, all bytes interpreted even if not understood...

--- parser.js
+++ parser.js
@@ -1,18 +1,21 @@
 'use strict';
 
+const _ = require('lodash');
 const fs = require('fs');
 const path = require('path');
 const util = require('util');
 const zlib = require('zlib');
 
 const GAME_TURN = new Buffer([0x9D, 0x2C, 0xE6, 0xBD]);
-const START_PLAYER = new Buffer([0xBB, 0x63, 0xE3, 0x4F]);
+const GAME_SPEED = new Buffer([0x99, 0xB0, 0xD9, 0x05]);
+const START_PLAYER = new Buffer([0x31, 0xEB, 0x88, 0x62]);
 const PLAYER_CIV = new Buffer([0x2F, 0x5C, 0x5E, 0x9D]);
 const PLAYER_CIV_TYPE = new Buffer([0xBE, 0xAB, 0x55, 0xCA]);
 const PLAYER_PASSWORD = new Buffer([0x6C, 0xD1, 0x7C, 0x6E]);
 const PLAYER_NAME = new Buffer([0xFD, 0x6B, 0xB9, 0xDA]);
 const PLAYER_CURRENT_TURN = new Buffer([0xCB, 0x21, 0xB0, 0x7A]);
 const END_PLAYER = new Buffer([0x58, 0xBA, 0x7F, 0x4C]);
+const END_UNCOMPRESSED = new Buffer([0, 0, 1, 0]);
 const COMPRESSED_DATA_START = new Buffer([1, 0, 0x78, 0x9C]);
 const COMPRESSED_DATA_END = new Buffer([0, 0, 0xFF, 0xFF]);
 
@@ -27,27 +30,33 @@ module.exports.parse = (filename, options) => {
 
   let state;
 
-  while (null !== (state = scan(buffer, state))) {
-    if (state.last4.equals(GAME_TURN)) {
-      result.gameTurn = {
-        pos: state.pos,
-        data: readInt(buffer, state)
-      }
-    } else if (state.last4.equals(START_PLAYER)) {
+  while (null !== (state = readState(buffer, state))) {
+    if (state.next4.equals(GAME_SPEED)) {
+      break;
+    }
+    state.pos++;
+  }
+
+  do {
+    const info = parseEntry(buffer, state);
+
+    if (info.marker.equals(GAME_TURN)) {
+      result.gameTurn = info;
+    } else if (info.marker.equals(START_PLAYER)) {
       result.players.push({
         pos: state.pos,
-        data: readPlayer(buffer, state)
+        data: readPlayer(info, buffer, state)
       });
-    } else if (state.last4.equals(COMPRESSED_DATA_START)) {
-      let outputFilename = null;
+    }
 
+    if (state.next4.equals(END_UNCOMPRESSED)) {
       if (options.outputCompressed) {
-        outputFilename = path.basename(filename) + '.bin';
+        readCompressedData(buffer, state, path.basename(filename) + '.bin');
       }
 
-      readCompressedData(buffer, state, outputFilename);
+      break;
     }
-  }
+  } while (null !== (state = readState(buffer, state)));
 
   return result;
 };
@@ -57,123 +66,199 @@ if (!module.parent) {
   if (!argv._.length) {
     console.log('Please pass the filename as the argument to the script.');
   } else {
-    console.log(util.inspect(module.exports.parse(argv._[0], argv), false, null));
+    const result = module.exports.parse(argv._[0], argv);
+    if (argv.simple) {
+      console.log(util.inspect(simplify(result), false, null));
+    } else {
+      console.log(util.inspect(result, false, null));
+    }
   }
 }
 
 // Parse helper functions
 
-function scan(buffer, state) {
+function simplify(result) {
+  let mapFn = _.mapValues;
+
+  if (_.isArray(result)) {
+    mapFn = _.map;
+  }
+
+  return mapFn(result, i =>{
+    if (i.data && !_.isObject(i.data)) {
+      return i.data;
+    }
+
+    return simplify(i.data || i);
+  });
+}
+
+function readState(buffer, state) {
   if (!state) {
     state = {
-      pos: 4,
-      last4: buffer.slice(0, 4)
+      pos: 0,
+      next4: buffer.slice(0, 4)
     };
   } else {
-    state.pos++;
-
-    if (state.pos >= buffer.length) {
+    if (state.pos >= buffer.length - 4) {
       return null;
     }
 
-    state.last4 = buffer.slice(state.pos - 4, state.pos);
+    state.next4 = buffer.slice(state.pos, state.pos + 4);
   }
 
   return state;
 }
 
-function readPlayer(buffer, state) {
-  const result = {};
+function parseEntry(buffer, state) {
+  const result = {
+    pos: state.pos,
+    marker: state.next4,
+    type: buffer.readUInt32LE(state.pos + 4)
+  };
 
-  while ((state = scan(buffer, state)) != null && !state.last4.equals(END_PLAYER)) {
-    if (state.last4.equals(PLAYER_PASSWORD)) {
-      result.password = {
-        pos: state.pos,
-        value: readString(buffer, state)
-      };
-    } else if (state.last4.equals(PLAYER_NAME)) {
-      result.name = {
-        pos: state.pos,
-        value: readString(buffer, state)
-      };
-    } else if (state.last4.equals(PLAYER_CURRENT_TURN)) {
-      result.isCurrentTurn = {
-        pos: state.pos,
-        value: readBoolean(buffer, state)
-      };
-    } else if (state.last4.equals(PLAYER_CIV)) {
-      result.civ = {
-        pos: state.pos,
-        value: readString(buffer, state)
-      }
-    } else if (state.last4.equals(PLAYER_CIV_TYPE)) {
-      result.civType = {
-        pos: state.pos,
-        value: readString(buffer, state)
-      }
-    } else if (state.last4.equals(COMPRESSED_DATA_START)) {
-      state.pos--;
-      break;
+  state.pos += 8;
+
+  if (result.marker.readUInt32LE() < 256) {
+    result.data = 'SKIP';
+  } else {
+    switch (result.type) {
+      case 1:
+        result.data = readBoolean(buffer, state);
+        break;
+
+      case 2:
+        result.data = readInt(buffer, state);
+        break;
+
+      case 3:
+      case 0x0A:
+      case 0x15:
+        result.data = 'UNKNOWN!';
+        state.pos += 12;
+        break;
+
+      case 5:
+        result.data = readString(buffer, state);
+        break;
+
+      case 6:
+        result.data = readUtfString(buffer, state);
+        break;
+
+      case 0x14:
+        result.data = 'UNKNOWN!';
+        state.pos += 16;
+        break;
+
+      case 0x0B:
+        result.data = 'UNKNOWN!';
+        state.pos += 28;
+        break;
+
+      default:
+        throw new Error('Error parsing: ' + JSON.stringify(result));
     }
   }
 
   return result;
 }
 
+function readPlayer(info, buffer, state) {
+  const result = {};
+
+  do {
+    if (state.next4.equals(END_UNCOMPRESSED)) {
+      break;
+    }
+
+    if (info === null) {
+      info = parseEntry(buffer, state);
+    }
+
+    if (info.marker.equals(END_PLAYER)) {
+      break;
+    }
+
+    if (info.marker.equals(PLAYER_PASSWORD)) {
+      result.password = info;
+    } else if (info.marker.equals(PLAYER_NAME)) {
+      result.name = info;
+    } else if (info.marker.equals(PLAYER_CURRENT_TURN)) {
+      result.isCurrentTurn = info;
+    } else if (info.marker.equals(PLAYER_CIV)) {
+      result.civ = info;
+    } else if (info.marker.equals(PLAYER_CIV_TYPE)) {
+      result.civType = info;
+    }
+
+    info = null;
+  } while ((state = readState(buffer, state)) != null);
+
+  return result;
+}
+
 function readString(buffer, state) {
+  const origState = _.clone(state);
   let result = null;
 
-  if (buffer.readUInt32LE(state.pos) === 5) {
-    state.pos += 4;
-    const strLen = buffer.readUInt16LE(state.pos);
-    state.pos += 2;
+  const strLen = buffer.readUInt16LE(state.pos);
+  state.pos += 2;
 
-    if (buffer.slice(state.pos, state.pos + 6).equals(new Buffer([0, 0x21, 1, 0, 0, 0]))) {
-      state.pos += 6;
-      result = buffer.slice(state.pos, state.pos + strLen - 1).toString(); // Ignore null terminator
-      state.pos += strLen;
-    }
-  } else {
-    console.log('Error reading string', state);
+  const strInfo = buffer.slice(state.pos, state.pos + 6);
+  //new Buffer([0, 0x21, 1, 0, 0, 0]))
+  if (strInfo[1] === 0 || strInfo[1] === 0x20) {
+    state.pos += 10;
+    result = 'Don\'t know what this kind of string is...';
+  } else if (strInfo[1] === 0x21) {
+    state.pos += 6;
+    result = buffer.slice(state.pos, state.pos + strLen - 1).toString(); // Ignore null terminator
+    state.pos += strLen;
+  }
+
+  if (result === null) {
+    throw new Error('Error reading string: ' + JSON.stringify(origState));
+  }
+
+  return result;
+}
+
+function readUtfString(buffer, state) {
+  const origState = _.clone(state);
+  let result = null;
+
+  const strLen = buffer.readUInt16LE(state.pos) * 2;
+  state.pos += 2;
+
+  if (buffer.slice(state.pos, state.pos + 6).equals(new Buffer([0, 0x21, 2, 0, 0, 0]))) {
+    state.pos += 6;
+    result = buffer.slice(state.pos, state.pos + strLen - 2).toString('ucs2'); // Ignore null terminator
+    state.pos += strLen;
+  }
+
+  if (result === null) {
+    throw new Error('Error reading string: ' + JSON.stringify(origState));
   }
 
   return result;
 }
 
 function readBoolean(buffer, state) {
-  let result = false;
-
-  if (buffer.readUInt32LE(state.pos) === 1) {
-    state.pos += 12;
-    return !!buffer[state.pos];
-  } else {
-    console.log('Error reading boolean', state);
-  }
-
+  state.pos += 8;
+  const result = !!buffer[state.pos];
+  state.pos += 4;
   return result;
 }
 
 function readInt(buffer, state) {
-  let result = null;
-
-  if (buffer.readUInt32LE(state.pos) === 2) {
-    state.pos += 12;
-    return buffer.readUInt32LE(state.pos);
-  } else {
-    console.log('Error reading int', state);
-  }
-
+  state.pos += 8;
+  const result = buffer.readUInt32LE(state.pos);
+  state.pos += 4;
   return result;
 }
 
 function readCompressedData(buffer, state, filename) {
-  const endPos = buffer.indexOf(COMPRESSED_DATA_END, state.pos);
-
-  if (filename) {
-    const compressedData = buffer.slice(state.pos - 2, buffer.indexOf(COMPRESSED_DATA_END, state.pos));
-    const uncompressedData = zlib.unzipSync(compressedData);
-    fs.writeFileSync(filename, uncompressedData);
-  }
-
-  state.pos = endPos;
+  const compressedData = buffer.slice(state.pos + 4, buffer.indexOf(COMPRESSED_DATA_END, state.pos));
+  const uncompressedData = zlib.unzipSync(compressedData);
+  fs.writeFileSync(filename, uncompressedData);
 }
